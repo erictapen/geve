8cfd7bbd1422eecf98414421b95d3aa1af3ab327
Justin Humm <justin.humm@posteo.de>
2019-11-21 09:07:48 +0100

    some steps

--- svg-builder.hs
+++ svg-builder.hs
@@ -12,14 +12,28 @@ svg content =
   <> with (svg11_ content) [Version_ <<- "1.1", Width_ <<- "200", Height_ <<- "200"]
 
 -- 30*30
-basicRect :: (Int, Int) -> Element
-basicRect (x, y) = rect_ [
-    X_ <<- showI x,
-    Y_ <<- showI y,
-    Width_ <<- "30",
-    Height_ <<- "30",
-    Fill_ <<- "none",
-    Stroke_ <<- "black" ]
+basicRect :: RealFloat a => (a, a) -> Element
+basicRect (x, y) = g_ [
+        Transform_ <<- translate x y
+    ]
+    $ rect_ [
+        Width_ <<- "30",
+        Height_ <<- "30",
+        Fill_ <<- "none",
+        Stroke_ <<- "black" 
+    ]
+
+fractalRect :: RealFloat a => (a, a) -> Int -> Element
+fractalRect _ 0 = mempty
+fractalRect (x, y) n = (g_ [
+        Transform_ <<- translate x y <> rotate 10
+    ]
+    $ rect_ [
+        Width_ <<- showI (30 - n),
+        Height_ <<- showI (30 - n),
+        Fill_ <<- "none",
+        Stroke_ <<- "black" 
+    ]) <> fractalRect (x,y) (n-1)
 
 basicRectGrid :: Element
 basicRectGrid = mconcat $
@@ -29,4 +43,6 @@ main :: IO ()
 main = do
     print $ svg $
         basicRectGrid
+     <> fractalRect (10, 10) 10
+
 
