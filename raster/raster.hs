95d856df32c10f3d7200c2f31ed998a0c40f0d12
Justin Humm <justin.humm@posteo.de>
2019-11-27 21:58:51 +0100

    remove unnecessary code

--- raster/raster.hs
+++ raster/raster.hs
@@ -12,42 +12,6 @@ svg content =
      doctype
   <> with (svg11_ content) [ Version_ <<- "1.1", Width_ <<- "200", Height_ <<- "200" ]
 
--- 30*30
-basicMask :: (Int, Int) -> Element
-basicMask (x, y) = rect_ [
-        X_ <<- (showI x),
-        Y_ <<- (showI y),
-        Width_ <<- "30",
-        Height_ <<- "30",
-        Fill_ <<- "white",
-        Stroke_ <<- "none" 
-    ]
-
--- mask which consists of regular masks, made to create "windows"
-basicRectGrid :: Element
-basicRectGrid = mask_ [ Id_ <<- "myMask" ] $
-    rect_ [
-        X_ <<- "0",
-        Y_ <<- "0",
-        Width_ <<- "200",
-        Height_ <<- "200",
-        Fill_ <<- "white"
-    ]
-    <> (mconcat $
-        P.map
-            basicMask
-            [ (x,y) | x<-[ 10, 50, 90, 130 ], y<-[ 10, 50, 90, 130 ] ])
-
-maskFront :: Element
-maskFront = rect_ [
-        X_ <<- "0",
-        Y_ <<- "0",
-        Width_ <<- "200",
-        Height_ <<- "200",
-        Fill_ <<- "none"
-        -- Mask_ <<- "url(#myMask)"
-    ]
-
 pathSegment :: RealFloat a => (a, a) -> Text
 pathSegment (x, y) = mA x y
     <> lA x (y+30)
