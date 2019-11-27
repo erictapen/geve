54788e5a14064b9c2561b72a8462dd015f63a3c7
Justin Humm <justin.humm@posteo.de>
2019-11-27 21:58:21 +0100

    working mask!

--- raster/raster.hs
+++ raster/raster.hs
@@ -48,7 +48,33 @@ maskFront = rect_ [
         -- Mask_ <<- "url(#myMask)"
     ]
 
+pathSegment :: RealFloat a => (a, a) -> Text
+pathSegment (x, y) = mA x y
+    <> lA x (y+30)
+    <> lA (x+30) (y+30)
+    <> lA (x+30) y
+    <> lA x y
+    <> z
+
+mask :: Element
+mask = path_
+    [
+        D_ <<- (
+            mA 0 0
+            <> lA 0 200
+            <> lA 200 200
+            <> lA 200 0
+            <> lA 0 0
+            <> z
+            <> " "
+            <> (mconcat $ P.map pathSegment [ (x,y) | x<-[ 10, 50, 90, 130 ], y<-[ 10, 50, 90, 130 ] ])
+        ) 
+        , Stroke_ <<- "none"
+        , Fill_ <<- "white"
+        , Style_ <<- "fill-rule:evenodd"
+    ]
+
 main :: IO ()
 main = do
-    print $ svg $ basicRectGrid <> circle_ [ R_ <<- "200" ] <> maskFront
+    print $ svg $ circle_ [ R_ <<- "200", Fill_ <<- "blue" ] <> mask  
 
