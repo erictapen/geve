23f70e84fae153b7d65978880635771b9b8a918f
Justin Humm <justin.humm@posteo.de>
2019-11-27 22:03:08 +0100

    refactor

--- raster/raster.hs
+++ raster/raster.hs
@@ -12,13 +12,15 @@ svg content =
      doctype
   <> with (svg11_ content) [ Version_ <<- "1.1", Width_ <<- "200", Height_ <<- "200" ]
 
-pathSegment :: RealFloat a => (a, a) -> Text
-pathSegment (x, y) = mA x y
+maskSegment :: RealFloat a => (a, a) -> Text
+maskSegment (x, y) = mA x y
     <> lA x (y+30)
     <> lA (x+30) (y+30)
     <> lA (x+30) y
     <> lA x y
     <> z
+
+boxCoordinates = [ (x,y) | x<-[ 10, 50, 90, 130 ], y<-[ 10, 50, 90, 130 ] ]
 
 mask :: Element
 mask = path_
@@ -31,7 +33,7 @@ mask = path_
             <> lA 0 0
             <> z
             <> " "
-            <> (mconcat $ P.map pathSegment [ (x,y) | x<-[ 10, 50, 90, 130 ], y<-[ 10, 50, 90, 130 ] ])
+            <> (mconcat $ P.map maskSegment boxCoordinates)
         ) 
         , Stroke_ <<- "none"
         , Fill_ <<- "white"
