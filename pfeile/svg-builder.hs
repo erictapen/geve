ef95b04d181256a8153feeb952f92a8ec72e83ba
Justin Humm <justin.humm@posteo.de>
2020-02-25 22:22:28 +0100

    pfeile: made 02.svg reproducible

--- pfeile/svg-builder.hs
+++ pfeile/svg-builder.hs
@@ -54,16 +54,25 @@ main =
       lazyWriteSvg f g = do
         fileExists <- doesFileExist f
         when (forceRewrite || not fileExists) $ writeSvg f g
-      mkTriangle (point, Y) = toElement $ Triangle point 60 15
-      mkTriangle (_, N) = mempty
    in do
-        writeSvg "01.svg" $ g_ [] $ mconcat $ P.map mkTriangle
-          $ P.zip [(Point x y) | y <- [0, 30 .. 200], x <- [0, 15 .. (15 * 11)]]
-          $ [Y, Y, Y, Y, Y, Y, Y, Y, Y, Y, Y, Y]
-            ++ [Y, Y, N, Y, N, N, Y, Y, N, Y, N, Y]
-            ++ [Y, N, Y, N, Y, Y, N, N, Y, N, Y, Y]
-            ++ [Y, Y, N, Y, N, N, Y, Y, N, Y, N, Y]
-            ++ [Y, Y, Y, Y, Y, Y, Y, Y, Y, Y, Y, Y]
-
-
-
+        writeSvg "01.svg" $
+          let mkTriangle (point, Y) = toElement $ Triangle point 60 15
+              mkTriangle (_, N) = mempty
+           in g_ [] $ mconcat $ P.map mkTriangle
+                $ P.zip [(Point x y) | y <- [0, 30 .. 200], x <- [0, 15 .. (15 * 11)]]
+                $ [Y, Y, Y, Y, Y, Y, Y, Y, Y, Y, Y, Y]
+                  ++ [Y, Y, N, Y, N, N, Y, Y, N, Y, N, Y]
+                  ++ [Y, N, Y, N, Y, Y, N, N, Y, N, Y, Y]
+                  ++ [Y, Y, N, Y, N, N, Y, Y, N, Y, N, Y]
+                  ++ [Y, Y, Y, Y, Y, Y, Y, Y, Y, Y, Y, Y]
+        writeSvg "02.svg"
+          $ g_
+            [ Transform_ <<- rotateAround (-45) 90 80
+            ]
+          $ mconcat
+          $ P.map
+            toElement
+            [ Triangle (Point 60 60) 160 (-60),
+              Triangle (Point 120 60) 160 (-60),
+              Triangle (Point 180 60) 160 (-60)
+            ]
