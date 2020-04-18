7cab7a5d9be5aca81e1a957ccae6902dfac6889e
Justin Humm <justin.humm@posteo.de>
2020-04-18 14:36:12 +0200

    build whole book pdf with a nix flake

--- flake.nix
+++ flake.nix
@@ -2,20 +2,54 @@
   description = "Results of 'Grafische Elemente & visuelle Effekte'";
 
   outputs =
-    { self,
-      nixpkgs }: rec {
-        defaultPackage.x86_64-linux = let
-	    inherit (nixpkgs.pkgs.haskell.packages.ghc865) mkDerivation;
-          in mkDerivation {
-            pname = "geve";
-            version = "0.1.0.0";
-            src = ./.;
-            isLibrary = false;
-            isExecutable = true;
-            executableHaskellDepends = [
-              base directory hsnoise svg-builder text
+    { self
+    , nixpkgs
+    }: rec {
+      defaultPackage.x86_64-linux = packages.x86_64-linux.pdf;
+      packages.x86_64-linux = let
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+        inherit (pkgs) stdenv;
+      in
+        {
+          geve = let
+            inherit (pkgs.haskell.packages.ghc865) mkDerivation base directory hsnoise svg-builder text;
+          in
+            mkDerivation {
+              pname = "geve";
+              version = "0.1.0.0";
+              src = ./.;
+              isLibrary = false;
+              isExecutable = true;
+              executableHaskellDepends = [
+                base
+                directory
+                hsnoise
+                svg-builder
+                text
+              ];
+              homepage = "https://erictapen.name/";
+              license = stdenv.lib.licenses.bsd3;
+            };
+          pdf = pkgs.runCommand "geve-pdf" {
+            buildInputs = with pkgs; [
+              librsvg # for rsvg-convert
+              pdftk
+              packages.x86_64-linux.geve
             ];
-            homepage = "https://erictapen.name/";
-            license = stdenv.lib.licenses.bsd3;
-          };
+          } ''
+            mkdir -p cache $out/pages
+
+            # generate SVG files with the haskell binary
+            geve
+
+            # convert every SVG page to a singlepage PDF file
+            for svgpage in cache/*.svg; do
+              rsvg-convert --format=pdf --output="$out/pages/$(basename $svgpage).pdf" "$svgpage"
+            done
+
+            # concat all the PDF documents to one final result
+            pdftk $out/pages/*.pdf cat output $out/geve.pdf
+          '';
+        };
     };
+}
--- shell.nix
+++ shell.nix
@@ -2,15 +2,20 @@ with import <nixpkgs> {};
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    (pkgs.haskellPackages.ghcWithPackages (p: with p; [ 
-      diagrams
-      SVGFonts
-      svg-builder
-      JuicyPixels
-      either-unwrap
-      hsnoise
-      Noise
-    ]))
+    (
+      pkgs.haskellPackages.ghcWithPackages (
+        p: with p; [
+          diagrams
+          SVGFonts
+          svg-builder
+          JuicyPixels
+          either-unwrap
+          hsnoise
+          Noise
+        ]
+      )
+    )
     stack
+    librsvg
   ];
 }
