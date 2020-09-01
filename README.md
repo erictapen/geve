3ddeb2ec770cc316adbb5426ade760367c3564fe
Justin Humm <justin.humm@posteo.de>
2020-09-01 17:09:32 +0200

    migrate to flake only nix workflow

--- README.md
+++ README.md
@@ -4,24 +4,20 @@
 
 This book is generated from source using the [Nix](https://nixos.org/nix) tool. If you have Nix installed, you can generate the book either as a normal Nix derivation or as an exerimental [Nix flake](https://github.com/NixOS/rfcs/pull/49). Both methodes have been tested against the `nixos-20.03` branch of [Nixpkgs](https://github.com/NixOS/nixpkgs/).
 
-### Buiding the normal Nix way
-
-If you have Nix installed, running `nix build` should be enough to generate the final PDF file. It can then be found in the `result-1` symlink.
-
 ### Building as a Nix flake
 
-If you use Nix with flake support, you can also build this project using `nix build`. The final PDF file can be found behind the `result` symlink.
+If you use Nix with flake support, you can build this project using `nix build github:erictapen/geve#pdf`. The final PDF file can be found behind the `result` symlink.
 
 ## Hacking
 
 If you want to hack on the project, you might want to use the [Stack](https://docs.haskellstack.org/en/stable/README/) build tool. Executing `stack run` should be enough to generate the svg files in the `cache` dir.
 
-If you are using Nix and want to skip the `stack` part, you should be fine with pulling all necessary dependencies via `nix-shell`, then run `runhaskell src/Main.hs`.
+If you are using Nix (with experimental flakes support enabled) and want to skip the `stack` part, you should be fine with pulling all necessary dependencies via `nix develop`, then run `runhaskell src/Main.hs`.
 
 ### The `cache` dir
 
 After running, the `cache` directory is populated with images. Most of the images are generated only if they can't be found in the cache, so if you modify the source code you might need to delete the files in `cache` before rerunning the program.
 
-### Code format
+### Code formatting
 
 The code is formatted with [ormolu](https://github.com/tweag/ormolu). To format every Haskell source file in the repository, you can run `ormolu --mode inplace $(find src/ -name '*.hs')`.
--- flake.nix
+++ flake.nix
@@ -4,39 +4,122 @@
   outputs =
     { self
     , nixpkgs
-    }: rec {
+    }: let
+      forAllSystems = f: nixpkgs.lib.genAttrs
+        [ "x86_64-linux" "i686-linux" "aarch64-linux" ]
+        (system: f system);
+      nixpkgsFor = forAllSystems (
+        system: import nixpkgs { inherit system; }
+      );
+    in
+      rec {
 
-      defaultPackage.x86_64-linux = packages.x86_64-linux.pdf;
+        defaultPackage = forAllSystems (system: packages.${system}.pdf);
 
-      packages.x86_64-linux = import ./default.nix { inherit nixpkgs; };
+        packages = forAllSystems (
+          system: let
+            pkgs = nixpkgsFor.${system};
+          in
+            rec {
+              geve = let
+                inherit (pkgs.haskell.packages.ghc865) mkDerivation base directory hsnoise svg-builder text;
+              in
+                mkDerivation {
+                  pname = "geve";
+                  version = "0.1.0.0";
+                  src = builtins.filterSource (path: _: builtins.match ".*\.nix|result.*|flake.lock" path == null) ./.;
+                  isLibrary = false;
+                  isExecutable = true;
+                  executableHaskellDepends = [
+                    base
+                    directory
+                    hsnoise
+                    svg-builder
+                    text
+                  ];
+                  homepage = "https://erictapen.name/";
+                  license = pkgs.stdenv.lib.licenses.bsd3;
+                };
 
-      devShell.x86_64-linux = let
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
-      in
-        pkgs.mkShell {
-          buildInputs = with pkgs; [
-            (
-              pkgs.haskellPackages.ghcWithPackages (
-                p: with p; [
-                  diagrams
-                  SVGFonts
-                  svg-builder
-                  JuicyPixels
-                  either-unwrap
-                  hsnoise
-                  Noise
-                ]
-              )
-            )
-            stack
-            librsvg # convert SVG to PDF files
-            inkscape
-            ormolu # haskell code formatting
-            pdftk
-            python3Packages.numpy
-            python3Packages.matplotlib
-          ];
-        };
+              pdf = pkgs.runCommand "geve-pdf" {
+                buildInputs = with pkgs; [
+                  inkscape
+                  pdftk
+                  geve
+                ];
+                FONTCONFIG_FILE = pkgs.makeFontsConf {
+                  fontDirectories = [ pkgs.fira ];
+                };
+              } ''
+                mkdir -p cache $out/pages
 
-    };
+                # generate SVG files with the haskell binary
+                geve
+
+                # convert every SVG page to a singlepage PDF file
+                for svgpage in cache/*.svg; do
+                  cp $svgpage $out/pages/$(basename $svgpage)
+                  inkscape --export-pdf="$out/pages/$(basename $svgpage).pdf" "$svgpage"
+                  inkscape --export-dpi=300 --export-background=ffffff --export-png="$out/pages/$(basename $svgpage).png" "$svgpage"
+                done
+
+                # concat all the PDF documents to one final result
+                pdftk $out/pages/*.pdf cat output $out/geve.pdf
+              '';
+
+              sputnik-auswahl = pkgs.runCommand "sputnik-auswahl-pdf" {
+                buildInputs = with pkgs; [ pdftk ];
+              } ''
+                mkdir -p $out
+
+                pdftk \
+                  ${pdf}/pages/page001.svg.pdf \
+                  ${pdf}/pages/page004.svg.pdf \
+                  ${pdf}/pages/page005.svg.pdf \
+                  ${pdf}/pages/page008.svg.pdf \
+                  ${pdf}/pages/page009.svg.pdf \
+                  ${pdf}/pages/page013.svg.pdf \
+                  ${pdf}/pages/page014.svg.pdf \
+                  ${pdf}/pages/page015.svg.pdf \
+                  ${pdf}/pages/page016.svg.pdf \
+                  ${pdf}/pages/page020.svg.pdf \
+                  ${pdf}/pages/page021.svg.pdf \
+                  ${pdf}/pages/page029.svg.pdf \
+                  ${pdf}/pages/page031.svg.pdf \
+                  cat output $out/sputnik.pdf
+              '';
+            }
+        );
+
+        devShell = forAllSystems (
+          system: let
+            pkgs = nixpkgsFor.${system};
+          in
+            pkgs.mkShell {
+              buildInputs = with pkgs; [
+                (
+                  pkgs.haskellPackages.ghcWithPackages (
+                    p: with p; [
+                      diagrams
+                      SVGFonts
+                      svg-builder
+                      JuicyPixels
+                      either-unwrap
+                      hsnoise
+                      Noise
+                    ]
+                  )
+                )
+                stack
+                librsvg # convert SVG to PDF files
+                inkscape
+                ormolu # haskell code formatting
+                pdftk
+                python3Packages.numpy
+                python3Packages.matplotlib
+              ];
+            }
+        );
+
+      };
 }
